dc81c2391628795e4e39a216c9e164a3f33365cc
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2023-05-08 16:01:12 +0300

    chore: export BreakpontsType

--- src/theming/index.tsx
+++ src/theming/index.tsx
@@ -2,6 +2,6 @@ import GlobalStyles from './GlobalStyles.style'
 import ThemeProvider from './ThemeProvider'
 import theme from './theme'
 
-export type { FontSizesType, FontWeightsType, TextTransformType } from './types'
+export type { BreakpointsType, FontSizesType, FontWeightsType, TextTransformType } from './types'
 export * as ThemingUtils from './utils'
 export { theme, GlobalStyles, ThemeProvider }
