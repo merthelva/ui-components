d34ddd287fce4ef152821e074c1c059f3d6c371b
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2023-05-09 22:52:12 +0300

    feat: update Typography styles

--- src/components/Typography/Typography.style.tsx
+++ src/components/Typography/Typography.style.tsx
@@ -4,14 +4,29 @@ import type { IHeadingProps, IParagraphProps, ITextProps } from './Typography.in
 
 const TextStyled = styled.span<ITextProps>`
   ${({ fontSize, fontWeight: weight, transform }) =>
-    ThemingUtils.getTypographyStyle({ fontSize, weight, transform })};
-  color: ${({ color }) => ThemingUtils.getColor(color || 'primary')};
+    ThemingUtils.getTypographyStyle({
+      fontSize: fontSize || 'text_md',
+      weight: weight || 'regular',
+      transform: transform || 'none',
+    })};
+  color: ${({ color, isDisabled }) =>
+    isDisabled ? ThemingUtils.getColor('gray300') : ThemingUtils.getColor(color || 'primary')};
+  ${({ isDisabled }) =>
+    Boolean(isDisabled) &&
+    css`
+      cursor: not-allowed;
+    `}
 `
 
 const ParagraphStyled = styled.p<IParagraphProps>`
   ${({ fontSize, fontWeight: weight, transform }) =>
-    ThemingUtils.getTypographyStyle({ fontSize, weight, transform })};
-  color: ${({ color }) => ThemingUtils.getColor(color || 'primary')};
+    ThemingUtils.getTypographyStyle({
+      fontSize: fontSize || 'text_md',
+      weight: weight || 'regular',
+      transform: transform || 'none',
+    })};
+  color: ${({ color, isDisabled }) =>
+    isDisabled ? ThemingUtils.getColor('gray300') : ThemingUtils.getColor(color || 'primary')};
   ${({ align }) =>
     css`
       text-align: ${align || 'left'};
@@ -20,48 +35,71 @@ const ParagraphStyled = styled.p<IParagraphProps>`
     css`
       word-break: ${breakWord || 'normal'};
     `};
+  ${({ isDisabled }) =>
+    Boolean(isDisabled) &&
+    css`
+      cursor: not-allowed;
+    `}
 `
 
 const HeadingStyled = styled.h1<IHeadingProps>`
-  ${({ renderAs, transform }) => {
+  ${({ renderAs, fontSize, transform }) => {
     switch (renderAs) {
       case 'h1':
-        return ThemingUtils.getTypographyStyle({ fontSize: 'header_lg', weight: 'bold', transform })
+        return ThemingUtils.getTypographyStyle({
+          fontSize: fontSize || 'header_lg',
+          weight: 'bold',
+          transform: transform || 'none',
+        })
       case 'h2':
-        return ThemingUtils.getTypographyStyle({ fontSize: 'header_md', weight: 'bold', transform })
+        return ThemingUtils.getTypographyStyle({
+          fontSize: fontSize || 'header_md',
+          weight: 'bold',
+          transform: transform || 'none',
+        })
       case 'h3':
         return ThemingUtils.getTypographyStyle({
-          fontSize: 'header_md',
+          fontSize: fontSize || 'header_md',
           weight: 'semibold',
-          transform,
+          transform: transform || 'none',
         })
       case 'h4':
         return ThemingUtils.getTypographyStyle({
-          fontSize: 'header_sm',
+          fontSize: fontSize || 'header_sm',
           weight: 'semibold',
-          transform,
+          transform: transform || 'none',
         })
       case 'h5':
         return ThemingUtils.getTypographyStyle({
-          fontSize: 'header_sm',
+          fontSize: fontSize || 'header_sm',
           weight: 'medium',
-          transform,
+          transform: transform || 'none',
         })
       case 'h6':
         return ThemingUtils.getTypographyStyle({
-          fontSize: 'header_xs',
+          fontSize: fontSize || 'header_xs',
           weight: 'medium',
-          transform,
+          transform: transform || 'none',
         })
       default:
-        return ThemingUtils.getTypographyStyle({ fontSize: 'header_lg', weight: 'bold', transform })
+        return ThemingUtils.getTypographyStyle({
+          fontSize: fontSize || 'header_lg',
+          weight: 'bold',
+          transform: transform || 'none',
+        })
     }
   }}
-  color: ${({ color }) => ThemingUtils.getColor(color || 'primary')};
+  color: ${({ color, isDisabled }) =>
+    isDisabled ? ThemingUtils.getColor('gray300') : ThemingUtils.getColor(color || 'primary')};
   ${({ align }) =>
     css`
       text-align: ${align || 'left'};
     `};
+  ${({ isDisabled }) =>
+    Boolean(isDisabled) &&
+    css`
+      cursor: not-allowed;
+    `}
 `
 
 export { TextStyled, ParagraphStyled, HeadingStyled }
