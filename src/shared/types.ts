b452b8b92b8c2f4dad698cd730fe558f44a3ea5f
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2023-05-24 22:24:22 +0300

    feat: add global type for Storybook "argTypes"

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,5 +2,6 @@ import type { ComponentSizeEnum, VariantTypeEnum } from './constants'
 
 type VariantType = keyof typeof VariantTypeEnum
 type ComponentSizeType = keyof typeof ComponentSizeEnum
+type DisabledArgType = Record<string, Record<'table', Record<'disable', boolean>>>
 
-export type { VariantType, ComponentSizeType }
+export type { VariantType, ComponentSizeType, DisabledArgType }
