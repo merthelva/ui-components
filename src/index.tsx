ccd86a1d32e2e2b5bec5e10ee06b3a77946c2582
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2023-05-25 15:58:21 +0300

    chore: add a11y checker tool

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 
 import { GlobalStyles, ThemeProvider } from 'theming'
 
-const root = createRoot(document.getElementById('root') as HTMLElement)
+// For a complete list of Axe rules, please refer to https://dequeuniversity.com/rules/axe/4.7
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  import('@axe-core/react').then(({ default: axe }) => axe(React, ReactDOM, 1000))
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <GlobalStyles />
     <ThemeProvider>
       <div>Hello React</div>
     </ThemeProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
 
 // If you want to start measuring performance in your app, pass a function
